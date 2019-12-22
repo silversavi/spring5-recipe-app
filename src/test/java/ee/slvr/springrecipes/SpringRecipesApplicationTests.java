ad35b3850bf51061f958cb31e4fc4c4090dd7ca2
Silver Savi <silver.savi@gmail.com>
2019-12-22 23:10:49 +0200

    initial commit

---
+++ src/test/java/ee/slvr/springrecipes/SpringRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.slvr.springrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
